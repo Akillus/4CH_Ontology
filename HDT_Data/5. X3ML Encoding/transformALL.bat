f31c037d7bd8ef24c71d6321e9f25ef4fda718ed
Akillus <achille.felicetti@gmail.com>
2023-02-23 11:37:35 +0100

    Add files via upload

---
+++ HDT_Data/5. X3ML Encoding/transformALL.bat
@@ -0,0 +1,10 @@
+
+
+java -jar x3ml-engine-2.1.0-exejar.jar -i CaterinaCornaroArtAnalysis\cornaro_art_analysis.xml -x CaterinaCornaroArtAnalysis\cornaro_art_analysis.x3ml -p CaterinaCornaroArtAnalysis\generator-policy.xml -o CaterinaCornaroArtAnalysis\cornaro_art_analysis.rdf 
+java -jar x3ml-engine-2.1.0-exejar.jar -i CaterinaCornaroScientificAnalysis\cornaro_scientific_analysis.xml -x CaterinaCornaroScientificAnalysis\cornaro_scientific_analysis.x3ml -p CaterinaCornaroScientificAnalysis\generator-policy.xml -o CaterinaCornaroScientificAnalysis\cornaro_art_analysis.rdf 
+java -jar x3ml-engine-2.1.0-exejar.jar -i Lampadistis\lampadistis.xml -x Lampadistis\lampadistis.x3ml -p Lampadistis\generator-policy.xml -o Lampadistis\lampadistis.rdf 
+
+
+
+
+pause
